58c628c14be04c64eb75d01ea3d7c55ad82fa20c
Admin <ludvig.hafstrom@gmail.ocm>
2026-05-04 14:57:49 +0200

    Nytt

--- backend/src/main/java/se/su/inlupp/ListGraph.java
+++ backend/src/main/java/se/su/inlupp/ListGraph.java
@@ -2,7 +2,6 @@ package se.su.inlupp;
 
 import java.util.*;
 
-
 //Grafen skall vara en enkel viktad oriktad graf implementerad med kopplingslistor
 
 public class ListGraph<T> implements Graph<T> {
@@ -31,7 +30,6 @@ public class ListGraph<T> implements Graph<T> {
       return false;
     }
     return true;
-
   }
 
   @Override
@@ -41,14 +39,21 @@ public class ListGraph<T> implements Graph<T> {
 	    }
 	  
 	  if (weight < 0) {
-		throw new IllegalArgumentException
+		throw new IllegalArgumentException();
 		}
-	  
-	  if (//hitta kant mellan noder)
-	  }
-	  
-    throw new UnsupportedOperationException("Unimplemented method 'connect'");
-    
+
+      Set<Edge<T>> edgesFrom1 = list.get(node1);
+      for (Edge<T> e : edgesFrom1) {
+          if (e.getDestination().equals(node2)) {
+              throw new IllegalStateException();
+          }
+      }
+      Set<Edge<T>> aEdges = list.get(node1);
+      Set<Edge<T>> bEdges = list.get(node2);
+
+      aEdges.add(new ListEdge<>(name, weight, node2));
+      bEdges.add(new ListEdge<>(name, weight, node1));
+
     // Om någon av noderna saknas i grafen skall undantaget NoSuchElementException genereras.
     //Om vikten är negativ skall undantaget IllegalArgumentException genereras. 
     //Om en kant redan finns mellan dessa två noder skall undantaget IllegalStateException genereras.
@@ -57,11 +62,17 @@ public class ListGraph<T> implements Graph<T> {
   @Override
   public void disconnect(T node1, T node2) {
 
-	  if (!list.containsKey(node1) || !list.containsKey(node2)){
-		  throw new NoSuchElementException();
-	  }
+      Edge<T> edge1 = getEdgeBetween(node1,node2); //Den anropar en metod längre ner.
+      Edge<T> edge2 = getEdgeBetween(node2,node1); //Behöver göra det på båda nod då kanten finns hos båda.
 
-	  throw new UnsupportedOperationException("Unimplemented method 'disconnect'");
+              if ( edge1 == null || edge2 ==null){
+                  throw new IllegalStateException();
+              }
+      Set<Edge<T>> aEdges = list.get(node1);
+      Set<Edge<T>> bEdges = list.get(node2);
+
+      aEdges.remove(edge1);
+      bEdges.remove(edge2);
 
 	  // Om någon av noderna saknas i grafen skall undantaget NoSuch-ElementException genereras. 
 	  //Om det inte finns någon kant mellan noderna skall undantaget IllegalStateException genereras
@@ -69,26 +80,25 @@ public class ListGraph<T> implements Graph<T> {
 
   @Override
   public void setConnectionWeight(T node1, T node2, int weight) {
-	  
-	  if (!list.containsKey(node1) || !list.containsKey(node2) || //om ingen kant finns mellan dem){
-		  throw new NoSuchElementException();
-	  }
-	  
-	  if (weight < 0) {
-			throw new IllegalArgumentException
-			}
-	  
-	  
-    throw new UnsupportedOperationException("Unimplemented method 'setConnectionWeight'");
-    
-    // Om någon av noderna saknas i grafen eller om ingen kant finns mellan dem skall undantaget NoSuchElementException genereras. 
-    //Om vikten är negativ skall undantaget IllegalArgumentException genereras
-    
+
+      Edge<T> edge1 = getEdgeBetween(node1,node2); //Den anropar en metod längre ner.
+      Edge<T> edge2 = getEdgeBetween(node2,node1); //Behöver göra det på båda nod då kanten finns hos båda.
+
+      if ( edge1 == null || edge2 ==null) {
+          throw new NoSuchElementException();
+
+      }
+      edge1.setWeight(weight);
+      edge2.setWeight(weight);
+
+      // Om någon av noderna saknas i grafen eller om ingen kant finns mellan dem skall undantaget NoSuchElementException genereras.
+      // Om vikten är negativ skall undantaget IllegalArgumentException genereras
+
   }
 
   @Override
   public Set<T> getNodes() {
-    throw new UnsupportedOperationException("Unimplemented method 'getNodes'");
+    return new HashSet<>(list.keySet());
   }
 
   @Override
@@ -97,9 +107,8 @@ public class ListGraph<T> implements Graph<T> {
 	  if (!list.containsKey(node)){
 		  throw new NoSuchElementException();
 	  }
-	  
-    throw new UnsupportedOperationException("Unimplemented method 'getEdgesFrom'");
-    
+      return new HashSet<>(list.get(node));
+
     //Om noden saknas i grafen skall undantaget NoSuchElementException genereras
   }
 
@@ -109,10 +118,14 @@ public class ListGraph<T> implements Graph<T> {
 	  if (!list.containsKey(node1) || !list.containsKey(node2)){
 			  throw new NoSuchElementException();
 		  }
-  
-  
-    throw new UnsupportedOperationException("Unimplemented method 'getEdgeBetween'");
-    
+
+      Set<Edge<T>> edgesFrom1 = list.get(node1);
+      for (Edge<T> e : edgesFrom1) {
+          if (e.getDestination().equals(node2)) {
+              return e;
+          }
+      }
+      return null;
     //Om någon av noderna saknas i grafen skall undantaget NoSuchElementException genereras
   }
   
