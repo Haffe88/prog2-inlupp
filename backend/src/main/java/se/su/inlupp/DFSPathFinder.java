82d25e6ed36de4f84d6460d0a1fc9086f5eb2ef6
clockworkmaze7-cyber <clockwork.maze7@gmail.com>
2026-05-05 19:36:31 +0200

    lagt till metoder i listpath, samt egna kommentarer för förståelse

--- backend/src/main/java/se/su/inlupp/DFSPathFinder.java
+++ backend/src/main/java/se/su/inlupp/DFSPathFinder.java
@@ -8,39 +8,52 @@ public class DFSPathFinder<T> implements PathFinder<T> {
   public Path<T> findPath(Graph<T> graph, T from, T to) {
 	  
 	  Map <T, T> connections = new HashMap<>();
+	  //connections visar varje nod och vilken nod vi kom ifrån (to, from)
 	  connect(graph, from, null, connections);
 
 	  if (!connections.containsKey(to)){
 		  return null;
 	  }
+	  //om to inte finns (ingen väg finns) - returnera null
+	  
 
 	  LinkedList<Edge<T>> path = new LinkedList<>();
 	  
 	  T current = to;
 	  
+	  //Går bakåt från målet (noden to) till starten (noden from)
+	  
 	  while(current != null && !current.equals(from)) {
 		  T next = connections.get(current);
+		  //hämtar nästa nod
 		  Edge<T> edge = graph.getEdgeBetween(next,current);
+		  //hämtar edge mellan noden next och current
 		  path.addFirst(edge);
+		  //lägger till edgen först i linked-listed path
 		  current = next;
+		  
 	  }
+	  //fortsätter tills når noden from
 	  
 	  return new ListPath<>(from, path);
+	  //returnera en Path om vägen finns, slutnoden finns implicit i sista edgen i path
   }
 	private void connect(Graph<T> graph, T to, T from, Map<T, T> connections) {
 		connections.put(to, from);
+		//lägger in to och from noder i connections hashmap
 
 		for (Edge<T> edge : graph.getEdgesFrom(to)){
 			T destination = edge.getDestination();
+			//går igenom edges i to-nod och kollar vilka noder de har (kolla grannar)
 
 			if (!connections.containsKey(destination)) {
 				connect(graph,destination, to, connections);
 			}
+			//om noden inte är besökt - gå dit rekursivt
 		}
 	}
 }
 
-//Påbörjade koden här men det krävs en path-klass som implementerar path-interfacet för att man ska kunna returnera här
 
 //Djupetförst-sökning för att hitta väg mellan två noder
 //skall returnera en Path<T> som representerar den hittade vägen, eller null om ingen väg finns.
--- backend/src/main/java/se/su/inlupp/ListPath.java
+++ backend/src/main/java/se/su/inlupp/ListPath.java
@@ -5,14 +5,69 @@ import java.util.List;
 
 public class ListPath<T> implements Path<T> {
 
-private final T start;
-private final List<Edge<T>> edges;
+	private final T start;
+	private final List<Edge<T>> edges;
+	private int totalWeight;
 
-public ListPath(T start, List<Edge<T>>edges){
-    this.start = start;
-     this.edges = new ArrayList<>(edges);
+	public ListPath(T start, List<Edge<T>>edges){
+		this.start = start;
+		this.edges = new ArrayList<>(edges);
 
-    }
+	}
+
+	@Override
+	public T getStart() {
+		return this.start;
+	}
+
+	@Override
+	public T getEnd() {
+		return edges.get(edges.size()-1).getDestination();
+		//hämtar sista edge (mha index) i edges-listan, hämtar sen noden (den sista nodens destination)
+	}
+	
+	
+	@Override
+	public int getTotalWeight() {
+		
+		int totalWeight = 0;
+		
+		for (Edge<T> edge: edges) {
+			totalWeight += edge.getWeight();
+		}
+		return totalWeight;
+	}
+	//ska returnera summa för alla edges vikter
+	
+	@Override
+	public List<Edge<T>> getEdges(){
+		return this.edges;
+	}
+	
+	
+	@Override
+	public List<T> getNodes(){
+
+		List<T> nodeList = new ArrayList<>();
+
+		nodeList.add(start);
+		//Lägger till startnoden, då den inte är med i edges
+
+		for (Edge<T> edge: edges) {
+			T node = edge.getDestination();
+			nodeList.add(node);
+		}
+		
+		//Lägger till noder från edges i nodeList
+
+		return nodeList;
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+	}
 
 }
 
