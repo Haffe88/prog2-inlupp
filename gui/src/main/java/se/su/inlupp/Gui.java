a859a6291c74d7961906325b69320e0ba816faa4
clockworkmaze7-cyber <clockwork.maze7@gmail.com>
2026-05-12 11:50:53 +0200

    kommentar

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -22,7 +22,7 @@ public class Gui extends Application {
     stage.setScene(scene);
     stage.show();
   }
-//Julias kommentar
+//Julias kommentar ny
   public static void main(String[] args) {
     launch(args);
   }
